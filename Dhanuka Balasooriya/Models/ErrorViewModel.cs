c833314308ac33662f245f4065edf6fa2840058d
Dhanuka Balasooriya <it20207236@my.sliit.lk>
2024-06-29 14:26:04 +0530

    Add project files.

---
+++ Dhanuka Balasooriya/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Dhanuka_Balasooriya.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
